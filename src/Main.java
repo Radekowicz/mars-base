e4e0a131f0afff3d0ad97237e3d2eb84feb0ea85
Damian Chrzanowski <damianchrzanek@gmail.com>
2019-04-25 15:42:21 +0200

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,8 @@
-import buildings.Building;
-import buildings.Greenhouse;
-import buildings.Hub;
-import buildings.PowerStation;
-import resources.ResourcePack;
-
-import java.util.ArrayList;
-import java.util.List;
+import simulator.Simulator;
 
 public class Main {
-
-
     public static void main(String[] args) throws Exception {
-
-        ResourcePack resourcePack = new ResourcePack(100, 100, 100);
-        List<Building> buildings = new ArrayList<>();
-        buildings.add(new Hub());
-        buildings.add(new PowerStation());
-        buildings.add(new Greenhouse());
-
-        Simulator simulator = new Simulator(resourcePack, buildings);
-
-        int counter = 0;
-        while(true) {
-            simulator.print();
-            simulator.tick();
-            Thread.sleep(1000);
-            counter++;
-            if(counter == 0) simulator.build(new PowerStation());
-            if(counter == 3) simulator.build(new Hub());
-            if(counter == 7) simulator.build(new Greenhouse());
-        }
+        Simulator marsBasesimulator = new Simulator();
+        marsBasesimulator.run();
     }
 }
