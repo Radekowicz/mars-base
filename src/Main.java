644af0aaaada6e6aff5c7a20591f54651e830f80
Damian Chrzanowski <damianchrzanek@gmail.com>
2019-05-14 20:30:12 +0200

    Delete Main.java

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import simulator.MarsBaseSimulator;
-
-public class Main {
-    public static void main(String[] args) {
-        MarsBaseSimulator marsBaseSimulator = new MarsBaseSimulator();
-        marsBaseSimulator.run();
-    }
-}
