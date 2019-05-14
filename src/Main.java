ef3e4e65f6d8b92d7b7cdbb1f8fabfc0ed9233a5
Damian Chrzanowski <damianchrzanek@gmail.com>
2019-05-14 20:43:29 +0200

    Revert "Delete Main.java"
    This reverts commit 644af0aaaada6e6aff5c7a20591f54651e830f80.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import simulator.MarsBaseSimulator;
+
+public class Main {
+    public static void main(String[] args) {
+        MarsBaseSimulator marsBaseSimulator = new MarsBaseSimulator();
+        marsBaseSimulator.run();
+    }
+}
